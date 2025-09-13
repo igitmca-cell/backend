3511a45bbe286b8ff569278bddafe1f2d8b2d661
Sidhanta Pradhan <sidd07dev@gmail.com>
2025-09-13 14:53:42 +0530

    face matching error fix

--- src/app.js
+++ src/app.js
@@ -11,13 +11,9 @@ app.use(cors({
 
 }))
 
-app.use(express.json({
-    limit:"16kb"
-}))
-app.use(express.urlencoded({
-    extended:true,
-    limit:"16kb"
-}))
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 app.use(express.static("public"))
 app.use(cookieParser())
 import cronJob from "./controllers/cronjob.controller.js";
